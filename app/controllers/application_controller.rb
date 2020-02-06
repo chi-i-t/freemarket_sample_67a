4bc6cb084049961b0cd50bc06c0be4220b11f89e
kengo9637 <boeing.kengo@gmail.com>
2020-02-06 15:09:48 +0900

    strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :bithday, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :birthday, :phone_number])
     end
 
   private
