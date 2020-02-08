42a3f4f71acebfdf011329c410ef35bda31a0883
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-02-09 05:23:30 +0900

    add users_column
    ・users テーブルにカラムを追加
    ・新規登録ページのフロント整備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :basic_auth, if: :production?
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  protected
 
-  #   def configure_permitted_parameters
-  #     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :birthday, :phone_number])
-  #   end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :birthday, :phone_number])
+    end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root "items#index"
 
   
-  resources :users, only: [:index, :new, :create, :edit, :show, :destroy, :update]
+  # resources :users, only: [:index, :new, :create, :edit, :show, :destroy, :update]
   resources :items, only: [:index, :new, :create, :edit, :show, :destroy, :update]
 end
