7d66d1ea324a69640bcea2b91acfc9b47d933901
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-02-08 16:25:52 +0900

    drop DB
    ##デバイスのコントローラーをアレンジ
    ユーザー新規登録ができないため
    （パスワードがゼロになってしまう）
    アレンジしても登録できず。
    
    ## DB(user、image)削除のための準備
    ### ルート
    デバイス箇所のコメントアウト
    ### コントローラ
    上記で作ったデバイス（user)コントローラー削除
    アプリケーションコントローラーのデバイス箇所のコメントアウト
    ### ビュー
    userディレクトリをmypageに変更
    deviseディレクトリのログイン、サインアップのトップ、サインアップ、計３ファイルをsign-upディレクトリの中に移動
    ### モデル
    imageモデル、テーブルを削除
    userモデル、テーブルを削除
    マイグレーションファイルも一緒に削除された。
    テーブルが既にないため、マイグレーションファイルをdownにできず。
    db:dropの作業へ
    
    ## DB削除
    rails db:drop後
    rails db:create
    ローカルのトップページ見れることを確認。
    マイグレーションファイルがhttps://i.gyazo.com/b59ae31f0281813d823b6b46e9cda407.png
    であることが気になる。
    手動で削除せず進める。

---
+++ db/migrate/20200207122231_add_column_to_users2.rb
@@ -0,0 +1,5 @@
+class AddColumnToUsers2 < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :phone_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,29 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20200205065716) do
 
-  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "family_name",                         null: false
-    t.string   "first_name",                          null: false
-    t.string   "family_name_kana",                    null: false
-    t.string   "first_name_kana",                     null: false
-    t.string   "nickname",                            null: false
-    t.string   "bithday",                             null: false
-    t.string   "phone_number"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
 end
