e33821ab7495e2e542e11377499c7350ade29151
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-02-11 17:13:32 +0900

    修正
    ・userのバリデーション
    ・userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :birthday, presence: true
 
    # 半角以外の正規表現      
   validates :family_name, :first_name,format: {
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,64 +11,63 @@ describe User do
     it "is invalid without a nickname" do
       user = build(:user, nickname: "")
       user.valid?
-      expect(user.errors[:nickname]).to include("can't be blank")  
+      expect(user.errors[:nickname]).to include("translation missing: ja.activerecord.errors.models.user.attributes.nickname.blank")  
     end
 
-    # 3.emailが空では登録できないこと
+    # 3. emailが空では登録できないこと
     it "is invalid without a email" do
       user = build(:user, email: "")
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.blank")
     end
 
     # 4.passwordが空では登録できないこと
     it "is invalid without a password" do
       user = build(:user, password: "")
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("translation missing: ja.activerecord.errors.models.user.attributes.password.blank")
     end
 
     # 5.passwordが存在してもpassword_confirmationが空では登録できないこと
     it "is invalid without a password_confirmation although with a password" do
       user = build(:user, password_confirmation: "")
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-    endude("can't be blank")
+      expect(user.errors[:password_confirmation]).to include("translation missing: ja.activerecord.errors.models.user.attributes.password_confirmation.blank")
     end
 
     # 6.family_nameが空では登録できないこと
     it "is invalid without a family_name" do
       user = build(:user, family_name: "")
       user.valid?
-      expect(user.errors[:family_name]).to include("can't be blank")
+      expect(user.errors[:family_name]).to include("translation missing: ja.activerecord.errors.models.user.attributes.family_name.blank")
     end
 
     # 7.first_nameが空では登録できないこと
     it "is invalid without a first_name" do
       user = build(:user, first_name: "")
       user.valid?
-      expect(user.errors[:first_name]).to include("can't be blank")
+      expect(user.errors[:first_name]).to include("translation missing: ja.activerecord.errors.models.user.attributes.first_name.blank")
     end
 
     # 8.family_name_kanaが空では登録できないこと
     it "is invalid without a family_name_kana" do
       user = build(:user, family_name_kana: "")
       user.valid?
-      expect(user.errors[:family_name_kana]).to include("can't be blank")
+      expect(user.errors[:family_name_kana]).to include("translation missing: ja.activerecord.errors.models.user.attributes.family_name_kana.blank")
     end
 
     # 9.first_name_kanaが空では登録できないこと
     it "is invalid without a first_name_kana" do
       user = build(:user, first_name_kana: "")
       user.valid?
-      expect(user.errors[:first_name_kana]).to include("can't be blank")
+      expect(user.errors[:first_name_kana]).to include("translation missing: ja.activerecord.errors.models.user.attributes.first_name_kana.blank")
     end
 
     # 10.birthdayが空では登録できないこと
     it "is invalid without a birthday" do
       user = build(:user, birthday: "")
       user.valid?
-      expect(user.errors[:birthday]).to include("can't be blank")
+      expect(user.errors[:birthday]).to include("translation missing: ja.activerecord.errors.models.user.attributes.birthday.blank")
     end
 
     # 11.passwordが７文字以上であれば登録できること
@@ -82,85 +81,71 @@ describe User do
     it "is invalid with a password that has less than 6 characters " do
       user = build(:user, password: "11111a", password_confirmation: "11111a")
       user.valid?
-      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+      expect(user.errors[:password]).to include("translation missing: ja.activerecord.errors.models.user.attributes.password.too_short")
     end
 
-    # 13. passwordが数字のみであれば登録できないこと
-    it "is invalid with a password that has numbers only " do
-      user = build(:user, password: "1111111", password_confirmation: "1111111")
-      user.valid?
-      expect(user.errors[:password]).to include("can't be numbers only")
-    end
-
-    # 14. passwordが英字のみであれば登録できないこと
-    it "is invalid with a password that has english letters only " do
-      user = build(:user, password: "aaaaaaa", password_confirmation: "aaaaaaa")
-      user.valid?
-      expect(user.errors[:password]).to include("can't be english letters only")
-    end
-
-    # 15. 重複したemailが存在する場合登録できないこと
+    # 13. 重複したemailが存在する場合登録できないこと
     it "is invalid with a duplicate email address" do
       user = create(:user)
       another_user = build(:user)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      expect(another_user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.taken")
     end
 
-    # 16. emailに@がなければ登録できないこと
+    # 14. emailに@がなければ登録できないこと
     it "emailに@なし" do
       user = build(:user, email: "kkkgmail.com")
       user.valid?
-      expect(user.errors[:email]).to include("@ there is not")
+      expect(user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.invalid")
     end
 
-    # 17. emailにドメインがなければ登録できないこと
+    # 15. emailにドメインがなければ登録できないこと
     it "emailにドメインなし" do
       user = build(:user, email: "kkkgmail")
       user.valid?
-      expect(user.errors[:email]).to include("no domain")
+      expect(user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.invalid")
     end
 
-   # 18. family_nameが半角であれば登録できないこと
+   # 16. family_nameが半角であれば登録できないこと
     it "family_nameが半角" do
       user = build(:user, family_name: "ｱa")
       user.valid?
-      expect(user.errors[:family_name]).to include("half-width characters are not allowed")
+      expect(user.errors[:family_name]).to include("全角のみで入力して下さい")
     end
 
-    # 19. first_nameが半角であれば登録できないこと
+    # 17. first_nameが半角であれば登録できないこと
     it "first_nameが半角" do
       user = build(:user, first_name: "ｱa")
       user.valid?
-      expect(user.errors[:first_name]).to include("half-width characters are not allowed")
+      expect(user.errors[:first_name]).to include("全角のみで入力して下さい")
     end
 
-    # 20. family_name_kanaが半角であれば登録できないこと
+    # 18. family_name_kanaが半角であれば登録できないこと
     it "family_name_kanaが半角" do
       user = build(:user, family_name_kana: "ｱｱ")
       user.valid?
-      expect(user.errors[:family_name_kana]).to include("half-width characters are not allowed")
+      expect(user.errors[:family_name_kana]).to include("全角カタカナのみで入力して下さい")
     end
 
-    # 21. first_name_kanaが半角であれば登録できないこと
+    # 19. first_name_kanaが半角であれば登録できないこと
     it "family_name_kanaが半角" do
       user = build(:user, family_name_kana: "ｱｱ")
       user.valid?
-      expect(user.errors[:family_name_kana]).to include("half-width characters are not allowed")
+      expect(user.errors[:family_name_kana]).to include("全角カタカナのみで入力して下さい")
     end
 
-    # 22. family_name_kanaが全角カナでなければ登録できないこと
+    # 20. family_name_kanaが全角カナでなければ登録できないこと
     it "family_name_kanaが全角カナ以外" do
       user = build(:user, family_name_kana: "ああａａ")
       user.valid?
-      expect(user.errors[:family_name_kana]).to include("cannot enter anything other than kana")
+      expect(user.errors[:family_name_kana]).to include("全角カタカナのみで入力して下さい")
     end
 
-    # 23. first_name_kanaが全角カナでなければ登録できないこと
+    # 21. first_name_kanaが全角カナでなければ登録できないこと
     it "first_name_kanaが全角カナ以外" do
       user = build(:user, first_name_kana: "ああａａ")
       user.valid?
-      expect(user.errors[:first_name_kana]).to include("cannot enter anything other than kana")
+      expect(user.errors[:first_name_kana]).to include("全角カタカナのみで入力して下さい")
     end
 
   end
