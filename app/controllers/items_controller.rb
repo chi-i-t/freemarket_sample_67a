08657b2437863b1879259390379493bc6ebf00a3
0kkun <shinji.okumoto.01@gmail.com>
2020-02-13 18:36:42 +0900

    Create category model
    seedファイルにancestoryで使用する
    parentとchildren、grand childrenのデータを記載。rake db:seedを実行するとデータベースにデータが入る。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new # Productクラスのインスタンスに関連づけられた、Imageクラスのインスタンスを作成
+    @parents = Category.all.order("id ASC").limit(13)
   end
 
   def create 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200213065504) do
+ActiveRecord::Schema.define(version: 20200213085350) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "prefecture_id"
     t.string   "city"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
