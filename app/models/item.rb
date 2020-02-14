4de6e239dff83116d46d49bd5d3dd4a7a1b3649e
0kkun <shinji.okumoto.01@gmail.com>
2020-02-14 17:25:52 +0900

    Edit varidation in item model
    テストコード実行の際にエラーが出るので一時的にfog_providerをコメントアウトしました

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,14 @@ class Item < ApplicationRecord
   # allow_destroy: trueで親のレコードが削除された場合に、関連付いている子のレコードも一緒に削除してくれます
   accepts_nested_attributes_for :images, allow_destroy: true
 
+  
+  validates :name,      length: { maximum: 40 }
+  validates :explain,   length: { maximum: 1000 }
+  validates :price,     numericality: { only_integr: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :delivery_way
