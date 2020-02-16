e0a1963540272b3a8657e9071cef23e5b54f4f07
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-02-16 22:24:14 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20200213085350) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.string   "customer_id", null: false
+    t.string   "card_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
+  end
+
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "ancestry"
@@ -70,5 +79,6 @@ ActiveRecord::Schema.define(version: 20200213085350) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
 end
