2dab04bb65b205217f22605d40910f5f8eb49cc0
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-02-16 14:12:38 +0900

    add card_front,mypage_link
    ・クレジットカード登録・削除・一覧のフロント作成
    ・マイページよりクレジットカード登録へ遷移し、一覧へ、その後マイページへ戻るように実装。
    ・マイページを整備（トップ画面に本人情報表示、ユーザー情報変更画面へ遷移、ログアウト機能）
    ・トップページのヘッダー整備（ログイン済みかでボタン表示変更）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root "items#index"
   
   resources :items, only: [:index, :new, :create, :edit, :show, :destroy, :update]
-  resources :cards, only: [:index, :new, :create, :edit, :show, :destroy, :update]
+  resources :cards, only: [:new, :create, :edit, :show, :destroy, :update]
+  resources :my_pages, only: [:index]
+
 end
