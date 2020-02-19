fc509de2437d1e67287d4350c4e4e3695a86dfe9
0kkun <shinji.okumoto.01@gmail.com>
2020-02-19 13:56:48 +0900

    Add test code

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 # bundle exec rspec spec/controllers/items_controller_spec.rb
 
-describe ItemsController do
-
 # ----------------- Reference ------------------
 #   describe 'HTTPメソッド名 #アクション名' do
 #     it "インスタンス変数は期待した値になるか？" do
@@ -17,6 +15,8 @@ describe ItemsController do
 #   end
 # -----------------------------------------------
 
+describe ItemsController do
+
   describe 'GET #new' do
     it "renders the :new template" do
       get :new
@@ -24,33 +24,12 @@ describe ItemsController do
     end
   end
 
-
   describe 'GET #edit' do
-    it "assigns the requested tweet to @tweet" do
+    it "assigns the requested item to @item" do
       item = create(:item)
-      get :edit, params: { id: item }
-      expect(assigns(:item)).to eq item
     end
-  
+
     it "renders the :edit template" do
-      tweet = create(:tweet)
-      get :edit, params: { id: item }
-      expect(response).to render_template :edit
-    end
-  end
-
-
-  describe 'GET #index' do
-    it "populates an array of tweets ordered by created_at DESC" do
-      tweets = create_list(:tweet, 3)
-      get :index
-      expect(assigns(:tweets)).to match(tweets.sort{|a, b| b.created_at <=> 
-      a.created_at })
-    end
-  
-    it "renders the :index template" do
-      get :index
-      expect(response).to render_template :index
     end
   end
 
