638e7dcaee98982b5a968f3221651c6924abee04
chi-i-t <59216455+chi-i-t@users.noreply.github.com>
2020-02-20 16:31:30 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,6 @@ class ItemsController < ApplicationController
 
   
   def index
-    # @items = Item.where(buyer_id: nil).includes(:images).last(3)
     @items = Item.all
   end
 
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -5,8 +5,6 @@ class PurchaseController < ApplicationController
   before_action :set_card, :set_item, only: [:index, :pay]
 
   def index
-    
-    card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to controller: "cards", action: "new"
     else
@@ -19,20 +17,18 @@ class PurchaseController < ApplicationController
   end
 
   def pay
-    card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
       :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
       :customer => @card.customer_id, #顧客ID
       :currency => 'jpy', #日本円
     )
-
     redirect_to action: 'done' #完了画面に移動
   end
 
   def done
-    @item_buyer = Item.find(params[:item_id])
-    @item_buyer.update(buyer_id: current_user.id)
+    @item = Item.find(params[:item_id])
+    @item.update(buyer_id: current_user.id)
   end
 
   private
